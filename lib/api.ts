5afcaf15a72a612328fe7d202bff67e45ad3fc3b
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-07-31 21:07:09 +0200

    should start from components

---
+++ lib/api.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import type { Note, NoteTag } from "../components/types/note";
+
+
+const BASE_URL = "https://notehub-public.goit.study/api";
+const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+interface FetchNotesParams{
+    page: number;
+    search: string;
+}
+
+interface FetchNotesResponse {
+    notes: Note[];
+    totalPages: number;
+    
+}
+
+ export interface CreateNoteParams {
+    title: string;
+    content: string;
+    tag: NoteTag;
+}
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        Authorization: `Bearer ${token}`,
+    }
+});
+
+const Per_Page = 10;
+
+
+
+export const fetchNotes = async ({ page, search }:FetchNotesParams): Promise<FetchNotesResponse> => {
+    const response = await api.get<FetchNotesResponse>("/notes", {
+        params: {
+            page,
+            perPage: Per_Page,
+            ...(search.trim() !== "" && { search }),
+        },
+    });
+
+    return response.data;
+}
+
+export const createNote = async (note: CreateNoteParams): Promise<Note> => {
+    const response = await api.post<Note>("/notes", note);
+
+    return response.data;
+}
+
+export const deleteNote = async (noteId: string): Promise<Note> => {
+    const response = await api.delete<Note>(`/notes/${noteId}`);
+
+    return response.data;
+}
